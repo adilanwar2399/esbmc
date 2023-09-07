053e1095f91a060f8a110aec28a5647ed8fd801c
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2023-09-07 19:08:47 +0100

    Update esbmc-wrapper.py

--- scripts/competitions/svcomp/esbmc-wrapper.py
+++ scripts/competitions/svcomp/esbmc-wrapper.py
@@ -269,7 +269,7 @@ def get_command_line(strat, prop, arch, benchmark, concurrency, dargs):
     if concurrency:
       command_line += "--no-pointer-check --no-bounds-check "
     else:
-      command_line += "--no-pointer-check --no-bounds-check --error-label ERROR --goto-unwind --unlimited-goto-unwind "
+      command_line += "--no-pointer-check --no-bounds-check --error-label ERROR --goto-unwind --unlimited-goto-unwind --interval-analysis "
   else:
     print("Unknown property")
     exit(1)
