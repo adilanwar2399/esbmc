26884a82f468b5db1deb6993df7c29a1a040e054
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2023-11-16 01:06:03 +0000

    [interval] enable use of more precise intervals

--- scripts/competitions/svcomp/esbmc-wrapper.py
+++ scripts/competitions/svcomp/esbmc-wrapper.py
@@ -286,7 +286,7 @@ def get_command_line(strat, prop, arch, benchmark, concurrency, dargs, esbmc_ci)
     if concurrency:
       command_line += "--no-pointer-check --no-bounds-check "
     else:
-      command_line += "--no-pointer-check --interval-analysis --no-bounds-check --error-label ERROR --goto-unwind --unlimited-goto-unwind "
+      command_line += "--no-pointer-check --interval-analysis --no-bounds-check --error-label ERROR --goto-unwind --unlimited-goto-unwind --interval-analysis-arithmetic --interval-analysis-bitwise --interval-analysis-wrapped --interval-analysis-extrapolate"
   elif prop == Property.datarace:
     # TODO: can we do better in case 'concurrency == False'?
     command_line += "--no-pointer-check --no-bounds-check --data-races-check --no-assertions "
